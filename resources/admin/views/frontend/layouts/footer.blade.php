561865060aa6da791762c2fd9389277ba0209606
Iftekhar <iftekharahmed12@gmail.com>
2015-12-08 22:59:12 +0530

    footer blade

--- resources/admin/views/frontend/layouts/footer.blade.php
+++ resources/admin/views/frontend/layouts/footer.blade.php
@@ -1,11 +1,11 @@
 <div class="footer">
 	<div class="footer-links">
-		<a href="aboutus.htm">ABOUT US</a>
-		<a href="advertise-with-us.htm">ADVERTISE & PARTNER WITH US</a>
+		<a href="{{ url('/about') }}">ABOUT US</a>
+		<a href="{{ url('/advertise') }}">ADVERTISE & PARTNER WITH US</a>
 		<img src="images/red-patch.png" width="38" height="48" alt="">
 		<a href="#">FAQS</a>
-		<a href="privacy.htm">PRIVACY POLICY</a>
-		<a href="contact.htm">CONTACT US</a>
+		<a href="{{ url('/privacy') }}">PRIVACY POLICY</a>
+		<a href="{{ url('/contact') }}">CONTACT US</a>
 	</div>                                    
 	<p>Copyright © 2015 indianedition.com.  All Rights Reserved.</p>
 </div>
